9f06192bf6e4b114e9a6d98c4b7b264bf9718f7f
yizhi <yizhi020208@gmail.com>
2021-07-26 16:20:33 +0800

    add gesture detector

--- lib/pages/tabs/home.dart
+++ lib/pages/tabs/home.dart
@@ -8,27 +8,42 @@ class homePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        paceNote(
-          paceNoteID: 00,
-          userID: 00,
-          title: "明日方舟",
-          note: "末日生存不服来战,突如其来的假期,突如其来的假期,突如其来的假期,突如其来的假期,突如其来的假期,突如其来的假期,",
-          photo: 'https://www.itying.com/images/flutter/2.png',
+        GestureDetector(
+          child: paceNote(
+            paceNoteID: 00,
+            userID: 00,
+            title: "明日方舟",
+            note: "末日生存不服来战,突如其来的假期,突如其来的假期,突如其来的假期,突如其来的假期,突如其来的假期,突如其来的假期,",
+            photo: 'https://www.itying.com/images/flutter/2.png',
+          ),
+          onTap: () {
+            print('jump to details of paceNote, such as senicspot');
+          },
         ),
-        paceNote(
-          paceNoteID: 01,
-          userID: 01,
-          title: "阚清子",
-          note: "突如其来的假期",
-          photo: 'https://www.itying.com/images/flutter/2.png',
+        GestureDetector(
+          child: paceNote(
+            paceNoteID: 01,
+            userID: 01,
+            title: "阚清子",
+            note: "突如其来的假期",
+            photo: 'https://www.itying.com/images/flutter/2.png',
+          ),
+          onTap: () {
+            print('jump to details');
+          },
         ),
-        paceNote(
-          paceNoteID: 02,
-          userID: 02,
-          title: "小众景点",
-          note: "心旷神怡",
-          photo: 'https://www.itying.com/images/flutter/2.png',
-        )
+        GestureDetector(
+          child: paceNote(
+            paceNoteID: 02,
+            userID: 02,
+            title: "小众景点",
+            note: "心旷神怡",
+            photo: 'https://www.itying.com/images/flutter/2.png',
+          ),
+          onTap: () {
+            print('jump to details');
+          },
+        ),
       ],
     );
   }
