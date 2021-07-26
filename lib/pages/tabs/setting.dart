8801d710624b1857517919411789d6ab06fc539b
yizhi <yizhi020208@gmail.com>
2021-07-26 22:59:00 +0800

    7.26final edit

--- lib/pages/tabs/setting.dart
+++ lib/pages/tabs/setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
 
 class settingPage extends StatefulWidget {
   settingPage({Key? key}) : super(key: key);
@@ -10,6 +11,117 @@ class settingPage extends StatefulWidget {
 class _settingPageState extends State<settingPage> {
   @override
   Widget build(BuildContext context) {
-    return Text('我是设置组件！');
+    return ListView(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL, // default
+            front: Container(
+              height: 200,
+              width: 400,
+              color: Colors.cyanAccent,
+              child: Text('Front'),
+            ),
+            back: Container(
+              color: Colors.deepOrange,
+              height: 200,
+              width: 400,
+              child: Text('Back'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL, // default
+            front: Container(
+              height: 200,
+              width: 400,
+              color: Colors.cyan,
+              child: Text('Front'),
+            ),
+            back: Container(
+              color: Colors.green,
+              height: 200,
+              width: 400,
+              child: Text('Back'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL, // default
+            front: Container(
+              height: 200,
+              width: 400,
+              color: Colors.red,
+              child: Text('Front'),
+            ),
+            back: Container(
+              color: Colors.lightBlue,
+              height: 200,
+              width: 400,
+              child: Text('Back'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL, // default
+            front: Container(
+              height: 200,
+              width: 400,
+              color: Colors.red,
+              child: Text('Front'),
+            ),
+            back: Container(
+              color: Colors.lightBlue,
+              height: 200,
+              width: 400,
+              child: Text('Back'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL, // default
+            front: Container(
+              height: 200,
+              width: 400,
+              color: Colors.red,
+              child: Text('Front'),
+            ),
+            back: Container(
+              color: Colors.lightBlue,
+              height: 200,
+              width: 400,
+              child: Text('Back'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL, // default
+            front: Container(
+              height: 200,
+              width: 400,
+              color: Colors.red,
+              child: Text('Front'),
+            ),
+            back: Container(
+              color: Colors.lightBlue,
+              height: 200,
+              width: 400,
+              child: Text('Back'),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/res/module/senicSpot.dart
+++ lib/res/module/senicSpot.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'auditState.dart';
-import 'designCourseAppTheme.dart';
+import 'package:vertical_card_pager/vertical_card_pager.dart';
 
 class scenicSpot extends StatefulWidget {
   int scenicSpotID = 0; //to find the senicspots of the paceNote
@@ -54,75 +54,64 @@ class scenicSpot extends StatefulWidget {
 class _scenicSpotState extends State<scenicSpot> {
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      splashColor: Colors.transparent,
-      child: SizedBox(
-          width: 280,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                child: Row(
-                  children: <Widget>[
-                    const SizedBox(
-                      width: 48,
-                    ),
-                    Expanded(
-                        child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.cyanAccent,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(16.0)),
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          const SizedBox(
-                            width: 48 + 24.0,
-                          ),
-                          Expanded(
-                              child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 16),
-                                  child: Text(
-                                    '景点内容',
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 16,
-                                      letterSpacing: 0.27,
-                                      color: DesignCourseAppTheme.darkerText,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ))
-                        ],
-                      ),
-                    ))
-                  ],
-                ),
-              ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-                  child: Row(
-                    children: <Widget>[
-                      ClipRRect(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(16.0)),
-                        child: AspectRatio(
-                            aspectRatio: 1.0,
-                            child: Image.network(
-                                'https://www.itying.com/images/flutter/1.png')),
-                      )
-                    ],
+    final List<String> titles = [
+      "RED",
+      "YELLOW",
+      "BLACK",
+      "CYAN",
+      "BLUE",
+      "GREY",
+    ];
+
+    final List<Widget> images = [
+      Container(
+        color: Colors.red,
+      ),
+      Container(
+        color: Colors.yellow,
+      ),
+      Container(
+        color: Colors.black,
+      ),
+      Container(
+        color: Colors.cyan,
+      ),
+      Container(
+        color: Colors.blue,
+      ),
+      Container(
+        color: Colors.grey,
+      ),
+    ];
+    return Container(
+        width: 400,
+        height: 800,
+        child: Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: VerticalCardPager(
+                        titles: titles, // required
+                        images: images, // required
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold), // optional
+                        onPageChanged: (page) {
+                          // optional
+                        },
+                        onSelectedItem: (index) {
+                          // optional
+                        },
+                        initialPage: 0, // optional
+                        align: ALIGN.CENTER // optional
+                        ),
                   ),
                 ),
-              ),
-            ],
-          )),
-    );
+              ],
+            ),
+          ),
+        ));
   }
 }
