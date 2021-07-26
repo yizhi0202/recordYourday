d1cd1b9859c3b5897ba2db5b0ed06cac71dd1620
yizhi <yizhi020208@gmail.com>
2021-07-26 16:13:45 +0800

    senicSpot class

---
+++ lib/res/module/senicSpot.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class senicSpot extends StatefulWidget {
+  senicSpot({Key? key}) : super(key: key);
+
+  @override
+  _senicSpotState createState() => _senicSpotState();
+}
+
+class _senicSpotState extends State<senicSpot> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
