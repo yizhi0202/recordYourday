a00696d62e7ef68d1daa8e1ca4173ddb54607bf2
yizhi <yizhi020208@gmail.com>
2021-07-26 20:58:15 +0800

    use senicspotclass

--- lib/pages/tabs/catagory.dart
+++ lib/pages/tabs/catagory.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_y/res/module/senicSpot.dart';
 
 class categoryPage extends StatefulWidget {
   categoryPage({Key? key}) : super(key: key);
@@ -8,22 +9,15 @@ class categoryPage extends StatefulWidget {
 }
 
 class _categoryPageState extends State<categoryPage> {
+  List<String> myphoto = ['https://www.itying.com/images/flutter/1.png'];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ListTile(
-          title: Text("我是分类组件！"),
-          leading: Icon(Icons.phone_android),
-        ),
-        ListTile(
-          title: Text("我是分类组件！"),
-          leading: Icon(Icons.phone_android),
-        ),
-        ListTile(
-          title: Text("我是分类组件！"),
-          leading: Icon(Icons.phone_android),
-        ),
+        scenicSpot(scenicSpotID: 1, userID: 2, photo: myphoto),
+        scenicSpot(scenicSpotID: 1, userID: 2, photo: myphoto),
+        scenicSpot(scenicSpotID: 1, userID: 2, photo: myphoto)
       ],
     );
   }
