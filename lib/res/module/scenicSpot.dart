ea8e86189e4c814931f99c7e75b94f9b1166670c
yizhi <yizhi020208@gmail.com>
2021-07-29 17:17:09 +0800

    edit scenicspot

--- lib/res/module/scenicSpot.dart
+++ lib/res/module/scenicSpot.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'auditState.dart';
-import 'package:flip_card/flip_card.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:getwidget/getwidget.dart';
 
@@ -58,49 +57,111 @@ class _scenicSpotState extends State<scenicSpot> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 400,
-        width: 200,
-        child: GFCard(
-          boxFit: BoxFit.cover,
-          imageOverlay: AssetImage('images/forest.jpg'),
-          title: GFListTile(
-            title: Text('Awesome scenicspot'),
+      height: 300,
+      width: 200,
+      child: Card(
+          child: Stack(
+        children: <Widget>[
+          Image.network(
+            '${widget.photo![0]}',
+            fit: BoxFit.cover,
           ),
-          content: Text(
-              "GFCards has three types of cards i.e, basic, with avataras and with overlay image"),
-          buttonBar: GFButtonBar(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ListTile(
-                      leading: ClipOval(
-                        child: Image.network(
-                          'https://www.itying.com/images/flutter/2.png',
-                          height: 40,
-                          width: 40,
-                        ),
+          Positioned(
+            child: Container(
+              height: 36,
+              width: 64,
+              child: Image.network(
+                '${widget.photo![0]}',
+                fit: BoxFit.cover,
+              ),
+            ),
+            left: 10,
+            top: 10,
+          ),
+          Positioned(
+            child: Text(
+              '${widget.introduction}',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            top: 10,
+            left: 74,
+          ),
+          Positioned(
+            top: 14,
+            left: 74,
+            child: Container(
+              width: 60,
+              height: 30,
+              child: ListTile(
+                leading: Container(
+                    height: 30,
+                    width: 30,
+                    child: ClipOval(
+                      child: Image.network(
+                        '${widget.profilePhoto}',
+                        height: 30,
+                        width: 30,
                       ),
-                      title: Text('${widget.nickName}'),
-                    ),
-                    flex: 1,
-                  ),
-                  Expanded(
-                    child: GFButton(
-                      onPressed: () {},
-                      text: 'View',
-                    ),
-                    flex: 1,
-                  ),
-                  Expanded(
-                    child: IconButton(
-                        onPressed: () {}, icon: FaIcon(FontAwesomeIcons.heart)),
-                    flex: 1,
-                  )
-                ],
-              )
-            ],
+                    )),
+                title: Text('${widget.nickName}'),
+              ),
+            ),
           ),
-        ));
+          Positioned(
+              top: 14,
+              right: 10,
+              child: Container(
+                  height: 20,
+                  width: 20,
+                  child: IconButton(
+                    icon: FaIcon(FontAwesomeIcons.heart),
+                    onPressed: () {},
+                  )))
+        ],
+      )),
+    );
+    // child: GFCard(
+    //   boxFit: BoxFit.cover,
+    //   imageOverlay: AssetImage('images/forest.jpg'),
+    //   title: GFListTile(
+    //     title: Text('Awesome scenicspot'),
+    //   ),
+    //   content: Text(
+    //       "GFCards has three types of cards i.e, basic, with avataras and with overlay image"),
+    //   buttonBar: GFButtonBar(
+    //     children: <Widget>[
+    //       Row(
+    //         children: <Widget>[
+    //           Expanded(
+    //             child: ListTile(
+    //               leading: ClipOval(
+    //                 child: Image.network(
+    //                   'https://www.itying.com/images/flutter/2.png',
+    //                   height: 40,
+    //                   width: 40,
+    //                 ),
+    //               ),
+    //               title: Text('${widget.nickName}'),
+    //             ),
+    //             flex: 1,
+    //           ),
+    //           Expanded(
+    //             child: GFButton(
+    //               onPressed: () {},
+    //               text: 'View',
+    //             ),
+    //             flex: 1,
+    //           ),
+    //           Expanded(
+    //             child: IconButton(
+    //                 onPressed: () {}, icon: FaIcon(FontAwesomeIcons.heart)),
+    //             flex: 1,
+    //           )
+    //         ],
+    //       )
+    //     ],
+    //   ),
+    // ));
   }
 }
